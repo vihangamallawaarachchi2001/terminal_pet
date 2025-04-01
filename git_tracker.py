29b82eabe01356b4d78bc7cc221937941be7d9a1
vihangamallawaarachchi2001 <vihanganethusara00@gmail.com>
2025-04-01 18:00:21 +0530

    chacke commit

--- git_tracker.py
+++ git_tracker.py
@@ -3,9 +3,14 @@ from datetime import datetime
 
 class GitTracker:
     def __init__(self, repo_path="."):
-        self.repo = git.Repo(repo_path)
+        try:
+            self.repo = git.Repo(repo_path)
+        except git.exc.InvalidGitRepositoryError:
+            self.repo = None  # No repo found
 
     def get_commit_count_today(self):
+        if not self.repo:
+            return 0  # Return 0 if no repo
         today = datetime.today().strftime("%Y-%m-%d")
         commits = list(self.repo.iter_commits(since=today))
         return len(commits)
